b3454099d65c81d8bad88bb66e164d1a6a62efde
romulosafe <romulosantosferreira@gmail.com>
2016-05-11 22:16:54 -0300

    Atualizacoes na view

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -1,8 +1,58 @@
 $(document).ready(function(){
+	
+	var gridMusic 		= $("#music");
+	var gridMusicTitle 	= $("#gridMusicTitle");
+	var search 			= $("#search");
+	
+	search.focus();
+	
+	search.autocomplete({
+		source: function(request,response){
+		    $.ajax({
+		        url: "http://localhost/Unimusic/geonames.json",
+		        dataType: "json",
+		        success: function(data){
 
-    $('#btn-search').click(musicSearch);
+		            response($.map(data.estados.filter(completeFilter), function(item){
+		                  return {
+		                      label: item.nome,
+		                      value: item.nome
+		                  };
+		            }));
+		        }
+		        /*
+		        data: {
+		            parametros da requisição em json
+		        }*/
+		    });
+		},
+		autoFocus: true	
+	});
+	
+	gridMusicTitle.html("Musicas mais acessadas");
+	
 
 });
+
+var musicAutoComplete = function(request,response){
+    $.ajax({
+        url: "http://localhost/Unimusic/geonames.json",
+        dataType: "json",
+        success: function(data){
+
+            response($.map(data.estados.filter(completeFilter), function(item){
+                  return {
+                      label: item.nome,
+                      value: item.nome
+                  };
+            }));
+        }
+        /*
+        data: {
+            parametros da requisição em json
+        }*/
+    });
+};
 
 var musicSearch = function () {
 
